a5ebc40632957834c8a72fdab09eb976adcd4751
Yusuf Ensar Cebeci <96048976+yusufensarcebeci@users.noreply.github.com>
2024-11-28 13:08:25 +0300

    Update commit

---
+++ assets/scripts/services/TweenManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('TweenManager')
+export class TweenManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
