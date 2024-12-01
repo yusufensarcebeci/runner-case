9c1e517853ab2f73c1640a7478316f6144e63821
Yusuf Ensar Cebeci <96048976+yusufensarcebeci@users.noreply.github.com>
2024-12-01 21:04:00 +0300

    Update commit

--- assets/scripts/player/PlayerController.ts
+++ assets/scripts/player/PlayerController.ts
@@ -3,6 +3,7 @@ import { SwipeDetector } from "../core/SwipeDetector";
 import { SwipeType } from "../enums/SwipeType";
 import { GameStateManager } from "../managers/GameStateManager";
 import { GameState } from "../enums/GameState";
+import { TweenManager } from "../services/TweenManager";
 
 const { ccclass, property } = _decorator;
 
@@ -15,12 +16,12 @@ export class PlayerController extends Component {
   private jumpHeight: number = 3;
   private isJumping: boolean = false;
   private isMoving: boolean = false;
-  start() {
+  protected start(): void {
     SwipeDetector.getInstance();
     SwipeDetector.subscribe(this.onSwipe.bind(this));
   }
 
-  onSwipe(swipeType: SwipeType) {
+  private onSwipe(swipeType: SwipeType) {
     if (GameStateManager.getCurrentState() !== GameState.GAME_RUNNING) return;
     switch (swipeType) {
       case SwipeType.RIGHT:
@@ -110,38 +111,4 @@ export class PlayerController extends Component {
       )
       .start();
   }
-
-  //   private movePlayer(direction: number): void {
-  //     const targetLane = this.currentLane + direction;
-
-  //     // Hedef alan sınırları kontrol ediliyor.
-  //     if (targetLane < 0 || targetLane >= this.lanes.length) return;
-
-  //     this.currentLane = targetLane;
-  //     const targetX = this.lanes[targetLane];
-  //     const targetPosition = new Vec3(targetX, this.node.position.y, this.node.position.z);
-
-  //     // X ekseni hareketi
-  //     tween(this.node)
-  //       .to(this.moveSpeed, { position: targetPosition }, { easing: "smooth" })
-  //       .start();
-  //   }
-
-  //   private jump(): void {
-  //     if (this.isJumping) return;
-  //     this.isJumping = true;
-
-  //     const currentPosition = this.node.position.clone();
-  //     const jumpUp = new Vec3(currentPosition.x, this.jumpHeight, currentPosition.z);
-  //     const jumpDown = new Vec3(currentPosition.x, 0, currentPosition.z);
-
-  //     // Y ekseni hareketi
-  //     tween(this.node)
-  //       .to(this.jumpDuration / 2, { position: jumpUp }, { easing: "quadOut" })
-  //       .to(this.jumpDuration / 2, { position: jumpDown }, { easing: "quadIn" })
-  //       .call(() => {
-  //         this.isJumping = false;
-  //       })
-  //       .start();
-  //   }
 }
--- assets/scripts/services/TweenManager.ts
+++ assets/scripts/services/TweenManager.ts
@@ -1,116 +1,40 @@
 import {
-    _decorator,
-    Color,
-    Component,
-    Node,
-    Sprite,
-    tween,
-    Tween,
-    UIOpacity,
-    Vec3,
-  } from "cc";
-  const { ccclass, property } = _decorator;
-  
-  @ccclass("TweenManager")
-  export class TweenManager extends Component {
-    static scaleBounce(
-      node: Node,
-      scaleFactor: number = 1.2,
-      duration: number = 0.1,
-      delay: number = 0,
-      onComplete?: Function
-    ): void {
-      Tween.stopAllByTarget(node);
-      node.setScale(new Vec3(1, 1, 1));
-      const originalScale = node.scale.clone();
-  
-      const tween = new Tween(node)
-        .to(duration, { scale: new Vec3(scaleFactor, scaleFactor, scaleFactor) })
-        .to(duration, { scale: originalScale })
-        .delay(delay)
-        .call(() => {
-          if (onComplete) onComplete();
-        })
-        .start();
-    }
-    
-    static errorVibrate(
-      node: Node,
-      vibrationAmount: number = 5,
-      duration: number = 0.1,
-      delay: number = 0,
-      alignment:string ,  
-      onComplete?: Function
-    ) {
-      Tween.stopAllByTarget(node);
-    
-      const originalPosition = node.position.clone();
-    
-      const vibrationVector = alignment === "H"
-        ? new Vec3(originalPosition.x + vibrationAmount, originalPosition.y, originalPosition.z)
-        : new Vec3(originalPosition.x, originalPosition.y + vibrationAmount, originalPosition.z);
-    
-      new Tween(node)
-        .to(duration, { position: vibrationVector })
-        .to(duration, {
-          position: alignment === "H"
-            ? new Vec3(originalPosition.x - vibrationAmount, originalPosition.y, originalPosition.z)
-            : new Vec3(originalPosition.x, originalPosition.y - vibrationAmount, originalPosition.z),
-        })
-        .to(duration, { position: vibrationVector })
-        .to(duration, { position: originalPosition })
-        .delay(delay)
-        .call(() => {
-          if (onComplete) onComplete();
-        })
-        .start();
-    }
-  
-    static scaleTween(
-      node: Node,
-      scaleFactor: number = 1,
-      duration: number = 0.5
-    ) {
-      const tween = new Tween(node)
-        .to(duration, { scale: new Vec3(scaleFactor, scaleFactor, scaleFactor) })
-        .start();
-    }
-  
-    static opacityTween(
-      node: Node,
-      opacityFactor: number = 0,
-      duration: number = 0.5
-    ) {
-      const tween = new Tween(node.getComponent(UIOpacity))
-        .to(duration, { opacity: opacityFactor })
-        .start();
-    }
-  
-    static tweenToWorldPosition(
-      node: Node,
-      targetPosition: Vec3,
-      duration: number,
-      onComplete: () => void = () => {}
-    ) {
-      const originalPosition = node.position.clone();
-  
-      new Tween(node)
-        .to(duration, { worldPosition: targetPosition }, { easing: "sineInOut" })
-        .call(() => {
-          if (onComplete) onComplete();
-        })
-        .start();
-    }
-  
-    static blinkTween(
-      node: Node,
-      duration: number = 0.2,
-      repeatCount: number = 1
-    ) {
-      const opacitySequence = new Tween(node.getComponent(UIOpacity))
-        .to(duration / 2, { opacity: 150 })
-        .to(duration / 2, { opacity: 0 });
-  
-      opacitySequence.repeat(repeatCount).start();
-    }
-  }
+  _decorator,
+  Color,
+  Component,
+  Node,
+  Sprite,
+  tween,
+  Tween,
+  UIOpacity,
+  Vec3,
+} from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("TweenManager")
+export class TweenManager extends Component {
+
+  //TODO
+
+  // static moveToPosition(
+  //   targetNode: any,
+  //   targetPos: Vec3,
+  //   duration: number,
+  //   onComplete: Function
+  // ): void {
+  //   tween(targetNode.position)
+  //     .to(
+  //       duration,
+  //       { x: targetPos.x, y: targetPos.y, z: targetPos.z },
+  //       {
+  //         onUpdate: (target: Vec3) => {
+  //           targetNode.setPosition(target.x, target.y, target.z);
+  //         },
+  //         onComplete: () => {
+  //           if (onComplete) onComplete();
+  //         },
+  //       }
+  //     )
+  //     .start();
+  // }
+}
